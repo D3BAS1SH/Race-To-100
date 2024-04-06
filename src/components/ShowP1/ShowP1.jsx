3e1ad31b62a22970c34a232606a69a66d1e2e9e2
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-04-06 13:09:47 +0530

    Player Value showing component Added But not completed.

--- src/components/ShowP1/ShowP1.jsx
+++ src/components/ShowP1/ShowP1.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import './ShowP1.scss'
 
 const ShowP1 = () => {
     const User1Vals=useSelector(state=>state.User1);
   return (
     <div className='Player1Card'>
-        {
-            User1Vals.map((element,index)=>{
-                return <p key={index}>{index+1} : {element}</p>
-            })
-        }
+      <div className='backgroundDrop'></div>
+      <div className="RecordHolder">
+        <p>Term:2</p>
+        <p>Term:2</p>
+        <p>Term:2</p>
+        <p>Term:2</p>
+        <p>Term:2</p>
+      </div>
     </div>
   )
 }
