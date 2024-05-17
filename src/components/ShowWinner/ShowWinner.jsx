57b75a0e400d0dae166c787e992559335d3b4b40
D3BAS1SH <Morning.Star00.111@protonmail.com>
2024-05-17 17:29:01 +0530

    Showing who is winner by only player 1 or player2.

--- src/components/ShowWinner/ShowWinner.jsx
+++ src/components/ShowWinner/ShowWinner.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './ShowWinner.scss'
+import { useSelector } from 'react-redux'
 
 const ShowWinner = () => {
+
+  const isTrue=useSelector(state=>state.isTrue)
+
   return (
     <div className='myCard'>
       <div className='Show-winner'>
         <h1>
-          WINNER IS 
+          WINNER IS PLAYER {isTrue?1:2}
         </h1>
       </div>
     </div>
